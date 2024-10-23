4e897516ac97db156bed880011dfa2548f91d712
machmaegal <aka.pleat371@passinbox.com>
2024-10-23 16:44:43 +0200

    data types done

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const spaghetti = `${s1} ${s2} ${s3} ${s4} ${s5}`;
 
 // Print out the concatenated string
+console.log(spaghetti);
 
 
 
@@ -22,10 +23,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const cameLtaiL = part1.slice(0,-1) + part1[part1.length -1].toUpperCase() + part2.slice(0,-1) + part2[part2.length -1].toUpperCase();
 
 // Print the cameLtaiL-formatted string
-
+//console.log(cameLtaiL);
 
 
 
@@ -35,9 +36,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tip = billTotal * 0.15
 
 // Print out the tipAmount
+//console.log(tip);
 
 
 
@@ -47,10 +49,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomInt = Math.floor(Math.random() * 10 + 1)
 
 // Print the generated random number
 
+console.log(randomInt);
 
 
 /*******************************************
@@ -61,16 +64,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
 
-const expression7 = a && a;
+const expression7 = a && a; // true
